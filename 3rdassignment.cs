860986135b61dacc6ba666e89a9c3d278515c07f
ANGELICASEGUI <165160266+ANGELICASEGUI@users.noreply.github.com>
2024-04-01 11:21:34 +0800

    Add files via upload

---
+++ 3rdassignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace _3rdassignment_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input Seconds: ");
+            string s= Console.ReadLine();
+            float S = float.Parse(s);
+
+            float StM = float.Parse(s) / 30;
+            float MtH = float.Parse(s) / 30;
+            float HtD = float.Parse(s) / 24;
+            float DtM = float.Parse(s) / 24;
+
+
+            Console.WriteLine("SecondtoMinutes: "+ StM + "Minutes");
+            Console.WriteLine("MinutestoHours: " + MtH + "Hours");
+            Console.WriteLine("HourstoDays: "+ HtD + "Days");
+            Console.WriteLine("DaystoMonths: " + DtM + "Months");
+        }
+    }
+}
